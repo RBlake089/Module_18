297430231dd31c69a9457935d2464e5e7b5a5b8d
rblake089 <rblake089@gmail.com>
2023-08-14 19:03:42 -0400

    Create Mongoose model for Reaction entity with reactionBody, username, and createdAt

---
+++ models/Reaction.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+// Define the schema for the Reaction model
+const reactionSchema = new mongoose.Schema({
+  // The body of the reaction, required and with a maximum length of 280 characters
+  reactionBody: {
+    type: String,
+    required: true,
+    maxlength: 280
+  },
+  // Username of the user who posted the reaction, required
+  username: {
+    type: String,
+    required: true
+  },
+  // Date when the reaction was created, defaulting to the current date
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+// Create the Reaction model using the reactionSchema
+const Reaction = mongoose.model('Reaction', reactionSchema);
+
+// Export the Reaction model to be used in other parts of the application
+module.exports = Reaction;
