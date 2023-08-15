234b414241f1707c8937089ef40e9bf25ce1eb64
rblake089 <rblake089@gmail.com>
2023-08-14 20:26:03 -0400

    Add configuration file for MongoDB connection and server port settings

---
+++ config/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    // MongoDB connection URL
+    mongoURI: 'mongodb://localhost:27017/social_network_db',
+  
+    // Port for the Express server to listen on
+    port: process.env.PORT || 3000
+  };
+  
